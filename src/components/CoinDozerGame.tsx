a5d00c02bfde998d4b0e66ef3398c521e3b09a36
dwangwk <danielwang.wk@gmail.com>
2024-07-13 20:46:18 +0200

    fix: amplitude

--- src/components/CoinDozerGame.tsx
+++ src/components/CoinDozerGame.tsx
@@ -69,7 +69,7 @@ const CoinDozerGame: React.FC = () => {
         worldRef.current.step(1 / 60);
     
         // Update pusher and image
-        const amplitude = 3;
+        const amplitude = 1;
         const frequency = 0.005;
         pusherRef.current.body.position.z = -4.5 + Math.sin(time * frequency) * amplitude;
         pusherRef.current.mesh.position.copy(pusherRef.current.body.position as unknown as THREE.Vector3);
