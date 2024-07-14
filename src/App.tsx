39d5163b68a51552c0b4afc6f1565da15777ec6b
0xhatsume <0xhatsume@gmail.com>
2024-07-14 06:10:01 +0200

    :sparkles: Add some more Near cross chain code

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import CoinDozerGame from './components/CoinDozerGame';
 import { WagmiProvider } from 'wagmi';
 import { config } from './config/walletconfig';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+
+//import { Wallet } from '@/wallets/near';
+//import { NetworkId, HelloNearContract } from '@/config';
+import { NearContext } from './NearContext';
 
 const queryClient = new QueryClient()
+
+//const wallet = new Wallet({ createAccessKeyFor: HelloNearContract, networkId: NetworkId });
 
 const App: React.FC = () => {
   return (
